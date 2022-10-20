db885b896ee581c3607661c62e6b36e59abf5262
Johnny Bravo <HaKmonkey@gmail.com>
2022-10-19 23:24:38 -0400

    Update project3.erl

--- Project3/project3.erl
+++ Project3/project3.erl
@@ -1,25 +1,22 @@
 -module(project3).
 
-- export([start/2, server/0, chord_node/1]).
+-export([start/2, server/1, chord_node/1]).
 
-
+get_id(Pid, M) ->
+    Hash = binary:decode_unsigned(crypto:hash(sha256,erlang:pid_to_list(Pid))),
+    Hash rem erlang:trunc(math:pow(2,M)).
 
 resolve_chord_id(Id, ChordSize, Step) ->
-    % Id is where we are CURRENTLY
-    % M is used to get chord size to check all positions
-    % StepAtom = erlang:list_to_atom(erlang:integer_to_list(Step)),
-    NextNode = whereis(erlang:list_to_atom(erlang:integer_to_list(Step))),
-    %NextStep = erlang:atom_to_list(list_to_integer(Step)) + 1,
-    %NextStepAtom = erlang:list_to_atom(erlang:integer_to_list(NextStep)),
-    if
-        NextNode == undefined ->
-            resolve_chord_id(Id, ChordSize, Step + 1 rem ChordSize);
-        true ->
-            %io:fwrite("NOT UNDEFINED"),
-            NextNode
+    chord_ring ! {resolve_pid, self(), Step},
+    receive
+        {id_result, NextNode} ->
+            if
+                NextNode == false ->
+                    resolve_chord_id(Id, ChordSize, Step + 1 rem ChordSize);
+                true ->
+                    NextNode
+            end
     end.
-
-
 
 make_finger_table(I, M, _, FingerTable) when I == M-1 ->
     FingerTable;
@@ -27,66 +24,53 @@ make_finger_table(I, M, Id, FingerTable) when I < M ->
     ChordSize = erlang:trunc(math:pow(2, M)),
     Step = erlang:trunc(Id + math:pow(2, I)),
     NextNode = resolve_chord_id(Id, ChordSize, Step),
-    % find if a pid exists for Step - if not, check next int until back at self
-    NewFingerTable = lists:append([{Step, NextNode}], FingerTable),
+    {_, NextNodePid} = NextNode,
+    NewFingerTable = lists:append([{Step, NextNodePid}], FingerTable),
     make_finger_table(I+1, M, Id, NewFingerTable).
 
-
-
 chord_node(M) ->
-    Id = binary:decode_unsigned(crypto:hash(sha256,erlang:pid_to_list(self())))
-        rem erlang:trunc(math:pow(2,M)),
+    Id = get_id(self(), M),
     FingerTable = make_finger_table(0, M, Id, []),
-    %io:format("~p   ~p~n", [self(), Id]).
+    io:format("~p   ~p~n", [self(), Id]),
     receive
         test ->
             io:fwrite("~p   ~p   ~p~n", [self(), Id, FingerTable])
     end,
     chord_node(M).
 
-
-
-%% will literally just sort the hash values (IDs) and generate and send
-%% finger tables to the nodes
-make_chord_ring(NodeList) ->
-    % keys are just all possible positions on the chord ring
-    %io:fwrite("~p~n", [erlang:registered()]),
-    %global:registered_names(),
-    io:fwrite("~p~n", [NodeList]).
-
-
-
-spawn_node(0, From, NodeList, _) ->
-    From ! {finished_spawning, NodeList};
-spawn_node(NumNodes,From, NodeList, M) when NumNodes > 0 ->
+spawn_node(0, _) ->
+    chord_ring ! finished_spawning;
+spawn_node(NumNodes, M) when NumNodes > 0 ->
     Pid = spawn(?MODULE, chord_node, [M]),
-    Id = binary:decode_unsigned(crypto:hash(sha256,erlang:pid_to_list(Pid)))
-        rem erlang:trunc(math:pow(2,M)),
-    register(erlang:list_to_atom(erlang:integer_to_list(Id)), Pid),
-    NewNodeList = lists:append([{Id, Pid}], NodeList),
-    spawn_node(NumNodes - 1, From, NewNodeList, M).
-
-
+    Id = get_id(Pid, M),
+    chord_ring ! {update_node_list, [{Id, Pid}]},
+    %register(get_node_name(Id), Pid),
+    spawn_node(NumNodes - 1, M).
 
 % the server is here just to manage the initalization of everything
-server() ->
+% server will manage a list of PIDs to IDs instead of registering to atoms
+server(NodeList) ->
     receive
         {spawn_nodes, NumNodes, M} ->
-            spawn_node(NumNodes, self(), [], M);
-        {finished_spawning, NodeList} ->
-            make_chord_ring(NodeList)
+            spawn_node(NumNodes, M),
+            NewNodeList = NodeList;
+        {update_node_list, [{Id, Pid}]} ->
+            NewNodeList = lists:append([{Id, Pid}], NodeList);
+        {resolve_pid, From, Step} ->
+            From ! {id_result, lists:keyfind(Step, 1, NodeList)},
+            NewNodeList = NodeList;
+        finished_spawning ->
+            io:fwrite("finished spawning~n"),
+            NewNodeList = NodeList
     end,
-    server().
+    server(NewNodeList).
 
-
-
-% NumNodes is the number of nodes to generate
 % NumRequests is the number of requests each node will make
 % TODO need to make sure to pass NumRequests as well
+% TODO as a new node is generated the tables for relevant nodes should update
 start(NumNodes, NumRequests) ->
     % M = erlang:trunc(math:ceil(math:sqrt(NumNodes))),
-    M = 4,
-    ChordSize = erlang:trunc(math:pow(2, M)),
-    io:fwrite("M: ~p   Chord: ~p   Requests: ~p~n", [M, ChordSize, NumRequests]),
-    register(chord_host, spawn(?MODULE, server, [])),
-    chord_host ! {spawn_nodes, NumNodes, M}.
+    M = 6,
+    io:fwrite("M: ~p   Requests: ~p~n", [M, NumRequests]),
+    register(chord_ring, spawn(?MODULE, server, [[]])),
+    chord_ring ! {spawn_nodes, NumNodes, M}.
